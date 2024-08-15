f303cce558d10dc126ef55f8fd0c55154be3cd28
Basile <franjosebasile@hotmail.com>
2024-08-15 14:28:16 +0200

    added plotting for binomial distribution

--- Stats/Discrete.py
+++ Stats/Discrete.py
@@ -13,6 +13,8 @@ Created on Sun Jul 28 20:53:23 2024
 """
 import numpy as np
 import pandas as pd
+import math
+
 
 def Tchebysheff(mu=0, sigma=1, k=1):
     if k <1:
@@ -43,4 +45,60 @@ def Bernoulli(data):
     print(f"Expected value is {p}")
     print(f"Variance is {p*(1-p)}")
     
+def Binomial(data,Y=1,N=1): 
+    """
+    Calculate the probability function of a Binomial distribution.
 
+    Parameters:
+    Data that is either 0 or 1
+    y: numbers of success we are looking for
+    n: indepedent measurements we do in the sample
+
+    Returns:
+    p(y): probability of reaching Y successes in sample
+    p: probability of success (1)
+    e: expected value
+    v: variance
+    
+    p(y,n): 3d plot
+    """
+    if not data.isin([0, 1]).all().all():
+        raise ValueError("x must be either 0 or 1.")
+    
+    size = len(data)
+    p=data.sum()/size #success or 1
+    prob= math.comb(N, Y)*(p**Y)*(1-p)**(N-Y)
+    E=N*p
+    V=N*p*(1-p)
+    
+    n_values = np.arange(1, size+1)  # Valores de n
+    y_values = np.arange(0, size+1)  # Valores de y
+    p_y_n = np.array([[math.comb(n_i, y_j) * (p**y_j) * ((1-p)**(n_i-y_j)) if y_j <= n_i else 0
+                 for n_i in n_values] for y_j in y_values])
+    
+    return prob,E,V,p,p_y_n,n_values,y_values
+
+def Geometric(data,Y=1): 
+    """
+    Calculate the probability function of a Geometric distribution.
+
+    Parameters:
+    Data that is either 0 or 1
+    Y: number of the trial on which the first success happens
+
+    Returns:
+    p(Y=y): probability of having the first success in Y
+    p: probability of success (1)
+    e: expected value
+    v: variance
+    """
+    if not data.isin([0, 1]).all().all():
+        raise ValueError("x must be either 0 or 1.")
+    
+    size = len(data)
+    p=data.sum()/size #success or 1
+    P_Y_y=((1-p)**(Y-1))*p
+    
+    print(f"Probability of having the first success in {Y} is {P_Y_y}")
+    print(f"Expected value is {1/p}")
+    print(f"Variance is {(1-p)/(p**2)}")
--- example_stats.py
+++ example_stats.py
@@ -8,22 +8,72 @@ Created on Wed Aug 14 19:40:45 2024
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
 from GetData.FrankfurtBoerse import AbrufData
-from GetData.Discrete import Tchebysheff
+from Stats.Discrete import Tchebysheff
+from Stats.Discrete import Bernoulli
+from Stats.Discrete import Binomial
+from Stats.Discrete import Geometric
+from mpl_toolkits.mplot3d import Axes3D
+import math
+from scipy.stats import binom
 
+# Get data --------------------------------------------------------------------
 aktie_or_ISIN="DE0005140008"
-df=AbrufData(aktie_or_ISIN=aktie_or_ISIN,split=False,dividends=True,Bezugsrechte=True,Date_von="01/01/2023"
-             ,Date_bis="01/08/2024")
-
+df=AbrufData(aktie_or_ISIN=aktie_or_ISIN,split=False,dividends=True,
+             Bezugsrechte=True,Date_von="01/01/2023",Date_bis="01/08/2024")
 df['Datum'] = pd.to_datetime(df['Datum'], format='%d.%m.%y')
 df['Schluss'] = pd.to_numeric((df['Schluss'].str.replace('.','')).str.replace(',','.'))
 
+# Calculate properties of this distribution -----------------------------------
 mu=np.average(df['Schluss'])
 sigma=np.sqrt(((df['Schluss']-mu)**2).sum())
 Tchebysheff(mu=mu,sigma=sigma,k=1)
 
-# Predict if tomorrow the price will go up or down 
+# Predict if tomorrow the price will go up or down ----------------------------
 df['Return_day'] = df['Schluss'] - df['Schluss'].shift(1)
-
 df_binary = (df['Return_day'] > 0).astype(int)
+Bernoulli(df_binary)
+
+# Probability of having n independent (in theory) days with positive return (y)
+N,Y=150,75
+prob,E,V,p,p_y_n,n_values,y_values=Binomial(df_binary,Y=Y,N=N)
+print(f"Probability of positive return of 75 days given we consider 150 is {prob}")
+if abs(prob-binom.pmf(Y, N, p)) >0.000001:
+    raise ValueError(f"Official library disagrees with {binom.pmf(Y, N, p)}")
+
+# Probability of having at least n independent (in theory) days with positive return (y)
+prob_Y= pd.DataFrame(np.arange(0,N+1), columns=['Number of succeses'])
+prob_Y['Probability'] = prob_Y['Number of succeses'].apply(
+    lambda i: Binomial(df_binary, Y=i, N=N)[0])
+
+
+# Plotting binomial distribution 3D
+prob,E,V,p,p_y_n,n_values,y_values=Binomial(df_binary)
+n, y = np.meshgrid(n_values, y_values)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(n, y, p_y_n, cmap='viridis')
+ax.set(xlabel='Número de ensayos (n)', ylabel='Número de éxitos (y)', zlabel='Probabilidad P(Y=y)', 
+       title='Distribución Binomial 3D', 
+       xlim=(min(n_values), max(n_values)), 
+       ylim=(min(y_values), max(y_values)), 
+       zlim=(0, np.max(p_y_n))) 
+plt.show()
+
+# Plotting binomial distribution 2D
+p_y_N = [math.comb(N, y_i) * (p**y_i) * ((1-p)**(N-y_i)) for y_i in y_values]
+p_n_Y= [math.comb(n_i, Y) * (p**Y) * ((1-p)**(n_i-Y)) for n_i in n_values]
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+ax1.plot(y_values, p_y_N, marker='o')
+ax1.set(xlabel='Number of successes (y)', ylabel='Probability P(y,n=N)', 
+        title=f'Probability of finding y successes for fixed n={N}')
+ax1.grid(True)
+ax2.plot(n_values, p_n_Y, marker='o')
+ax2.set(xlabel='Number of Trials (n)', ylabel='Probability P(Y=y,n)',
+        title=f'Probability of finding {Y} successes for a given n')
+ax2.grid(True)
+plt.tight_layout()
+plt.show()
+
+# Probability of having the first success in the Y day
+Geometric(df_binary,Y=3)
